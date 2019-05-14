dea52f677d0e49054e72c600760a6a808c58ed45
Hiep0502 <dthiep.gm@gmail.com>
2019-05-14 01:04:45 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.write('<h2> Hello World </h2>');
+
+var h1 = document.querySelector('h1');
+h1.innerText = 'Hoc JavaScript trong 1 gio';
+
+h1.innerHTML ='<p>kakakkakak kakakakaka kakakaka</p>';
+h1.innerText ='<p>HELLO HELLO</p>';
+h1.textContent = 'gdfghigor';
